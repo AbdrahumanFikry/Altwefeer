02b70e50e93433f7e783939f19522fc3f6a7f6a1
AbdrahumanFikry <op.op25@yahoo.com>
2020-02-15 16:56:16 +0200

    Abdrahuman

--- lib/widgets/subCategorySelector.dart
+++ lib/widgets/subCategorySelector.dart
@@ -16,28 +16,36 @@ class SubCategorySelector extends StatelessWidget {
     return Container(
       height: 60.0,
       color: selected == index ? Colors.blue[50] : Colors.white,
-      child: Row(
-        children: <Widget>[
-          Container(
-            height: 60.0,
-            width: 7.0,
-            color: selected == index ? Colors.blue[800] : Colors.white,
-          ),
-          SizedBox(
-            width: 10.0,
-          ),
-          FittedBox(
-            child: Text(
-              title,
-              style: TextStyle(
-                color: selected == index ? Colors.blue[800] : Colors.black,
-                fontFamily: 'Roboto',
-              ),
-              softWrap: true,
-              overflow: TextOverflow.ellipsis,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          right: 1.0,
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              height: 60.0,
+              width: 7.0,
+              color: selected == index ? Colors.blue[800] : Colors.white,
             ),
-          ),
-        ],
+            SizedBox(
+              width: 10.0,
+            ),
+            Expanded(
+              child: Container(
+                child: Text(
+                  title,
+                  style: TextStyle(
+                    color: selected == index ? Colors.blue[800] : Colors.black,
+                    fontFamily: 'Roboto',
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  softWrap: true,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
